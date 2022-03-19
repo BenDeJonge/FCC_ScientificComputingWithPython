d36904d4b4fc4ef11dd668697e503e8aa1aa74ce
Ben De Jonge (imec) <74603987+BenDeJonge@users.noreply.github.com>
2022-03-19 23:50:46 +0100

    Update arithmetic_arranger.py
    - Check amount of problems.
    - Loop over all arithmatic problems to concatenate all lines together.
    - Problem and solution can be output separately.
    - If any problem returns an error, that error is displayed instead.

--- Project1/arithmetic_arranger.py
+++ Project1/arithmetic_arranger.py
@@ -1,4 +1,41 @@
-def arithmetic_arranger(problems):
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 22:46:13 2022
 
+@author: dejong71
+"""
 
-    return arranged_problems
+#______________________________________________________________________________
+
+from Project1.arithmaticproblem import ArithmaticProblem
+
+#______________________________________________________________________________
+
+def arithmetic_arranger(problems : list, show_solution=False):
+    # Avoiding printing too many problems.
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    # Storing problems in memory.
+    aps = [ ArithmaticProblem(p) for p in problems ]
+    # If no problem returns an error, group the outputs in a large string.
+    if all([ ap.quality_check for ap in aps ]):       
+        lines = ['' for i in range(4)]
+        for ap in aps:
+            for i, line in enumerate(ap.formatted_problem):
+                lines[i] += line + 4 * ' '
+        lines = [line.rstrip() for line in lines ]
+        # Printing problem and solution separately.
+        for line in lines[:-1]:
+            print(line)
+        if show_solution:
+            print(lines[-1])
+        return '\n'.join(lines)
+    # If any problem returns an error, display the first error.
+    else:
+        for ap in aps:
+            if ap.error != '':
+                if show_solution:
+                    print(ap.error)
+                return ap.error
+            
+#______________________________________________________________________________
