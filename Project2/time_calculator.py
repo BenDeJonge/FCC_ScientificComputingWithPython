d5b562d1522a7861ac1968e5055e4915ebf4e8bb
Ben De Jonge (imec) <74603987+BenDeJonge@users.noreply.github.com>
2022-03-21 10:09:16 +0100

    Update time_calculator.py
    - Output as f-string.
    - Minor formatting.

--- Project2/time_calculator.py
+++ Project2/time_calculator.py
@@ -1,7 +1,22 @@
-def add_time(start, duration):
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 19:07:55 2022
 
+@author: dejong71
+"""
+#______________________________________________________________________________
 
+import Project2.helper_functions as hf
 
+#______________________________________________________________________________
 
+def add_time(start, duration, start_day=None):
+    t1, t2 = map(hf.extract_hour_min, (start, duration))
+    ttot = hf.sum_time(t1, t2)
+    ttot = hf.find_period(ttot)
+    day_msg = hf.format_days_passed_msg(ttot)
+    curr_day_msg = hf.format_current_day_msg(start_day, ttot)
 
-    return new_time
+    return f'{ttot["hour"]}:{ttot["min"]:02} {ttot["period"]}{curr_day_msg}{day_msg}'
+
+#______________________________________________________________________________
